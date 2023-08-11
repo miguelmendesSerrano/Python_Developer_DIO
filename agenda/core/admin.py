8aa3d5750d958250d2675221d8b862162f6393ea
Miguel Mendes Serrano <mgl.mendes21@gmail.com>
2023-08-11 12:20:44 -0300

    Criando template evento.html e adicionando funcionalidades de edição e exclusão de eventos

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -5,7 +5,7 @@ from core.models import Evento
 
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "data_evento")
+    list_display = ("id", "titulo", "data_evento")
     list_filter = ("usuario", "titulo",)
 
 
